48c3527aff92b46e28546f1787f729cb3dd59844
Abhinand S S <abhinand@yougotagift.com>
2023-11-30 10:34:25 +0530

    Signup using cbv completed and proceeding to post section.

--- account/views.py
+++ account/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.views import LoginView
-from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from django.http import HttpResponse
 from .forms import SignUpForm
 from .models import UserProfile
-from django.shortcuts import render
 
 
-class InstagramLoginView(LoginView, TemplateView):
+class InstagramLoginView(LoginView):
     template_name = 'login_register.html'
 
     def form_valid(self, form):
@@ -21,48 +21,21 @@ class InstagramLoginView(LoginView, TemplateView):
 class InstagramSignupView(CreateView):
     template_name = 'login_register.html'
     form_class = SignUpForm
-
-
-    def get_form(self, form_class=form_class):
+    model = UserProfile
+    success_url = reverse_lazy('login')
 
     def form_invalid(self, form):
+        print(form.errors)
         super().form_valid(form)
         return HttpResponse('Registration failed!!')
-    
-    
+
     def form_valid(self, form):
         super().form_valid(form)
+        bio = form.cleaned_data.get('bio', '')
+        profile_pic = form.cleaned_data.get('profile_pic', '')
+        user = form.save(commit=False)
+        user.save()
+        profile = UserProfile.objects.create(user=user, bio=bio,
+                                             profile_pic=profile_pic)
         return HttpResponse('User registration successful!!')
-        
-    def post(self, request, *args, **kwargs):
-        if request.FILES:
-            files = request.FILES
-        else:
-            files = None
-        form = self.form_class(request.POST, files)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.username = user.username.lower()
-            user.save()
-            bio = form.cleaned_data['bio']
-            profile_pic = form.cleaned_data['profile_pic']
-            profile = UserProfile.objects.create(user=user, bio=bio,
-                                                 profile_pic=profile_pic)
-            profile.save()
-            return HttpResponse('User registration successful!!')
-        else:
-            return HttpResponse('Registration failed!!')
 
-
-    def get(self, request, *args, **kwargs):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
-
-
-# class CreatePostView(LoginRequiredMixin, CreateView):
-#     model = Post
-#     form_class = PostForm
-#     template_name = 'post_creation.html'
-#
-#     def form_valid(self, form):
-#         form.instance.user = self.request.user
